9c3518b6b1c05e0d73c7a3c20de05abf73b304ea
devgis <devgis@163.com>
2022-05-17 14:57:17 +0800

    Public
    Public

---
+++ Source/NPOIDemo/NPOIDemo/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using NPOI.XWPF.UserModel;
+
+namespace NPOIDemo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sfd = new OpenFileDialog();
+            sfd.Title = "Word2007文件";
+            sfd.FileName = "";
+            sfd.Filter = "Word2007文件(*.docx)|*.docx";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                using (FileStream stream = File.OpenRead(sfd.FileName))
+                {
+                    XWPFDocument doc = new XWPFDocument(stream);
+                    var tables = doc.Tables;
+                    foreach (var table in tables)    //遍历表格  
+                    {
+                        if (table.Rows.Count > 0)
+                        {
+                            var row0 = table.GetRow(0);
+                            for (int i = 0; i < row0.GetTableCells().Count;i++)
+                            {
+                                var c0 = row0.GetCell(i);        //获得单元格0  
+                                foreach (var para in c0.Paragraphs)
+                                {
+                                    string text = para.ParagraphText;
+                                    //处理段落      
+                                    sb.Append(text + ",");
+                                }
+                            }
+                        }
+                        sb.AppendLine("");
+                        continue;
+                        foreach (var row in table.Rows)    //遍历行  
+                        {
+                            var c0 = row.GetCell(0);        //获得单元格0  
+                            foreach (var para in c0.Paragraphs)
+                            {
+                                string text = para.ParagraphText;
+                                //处理段落      
+                                sb.Append(text + ",");
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show(sb.ToString());
+
+            }
+                
+        }
+    }
+}
---
+++ Source/WordDemo/WordDemo/Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace WordDemo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sfd = new OpenFileDialog();
+            sfd.Title = "Word2003文件";
+            sfd.FileName = "";
+            sfd.Filter = "Word2003文件(*.doc)|*.doc";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //实例化COM
+                Microsoft.Office.Interop.Word.ApplicationClass wordApp = new Microsoft.Office.Interop.Word.ApplicationClass();
+                object fileobj = sfd.FileName;
+                object nullobj = System.Reflection.Missing.Value;
+                //打开指定文件（不同版本的COM参数个数有差异，一般而言除第一个外都用nullobj就行了）
+                Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref fileobj, ref nullobj, ref nullobj,
+                    ref nullobj, ref nullobj, ref nullobj,
+                    ref nullobj, ref nullobj, ref nullobj,
+                    ref nullobj, ref nullobj, ref nullobj, ref nullobj, ref nullobj, ref nullobj, ref nullobj
+                    );
+                //取得doc文件中的文本
+                string outText = doc.Content.Text;
+
+                MessageBox.Show(ReadWord(doc, 1, 1, 1));
+                //关闭文件
+                doc.Close(ref nullobj, ref nullobj, ref nullobj);
+                //关闭COM
+                wordApp.Quit(ref nullobj, ref nullobj, ref nullobj);
+                //返回
+                //MessageBox.Show(outText);
+
+            }
+        }
+
+        /// <summary>
+        /// 返回指定单元格中的数据
+        /// </summary>
+        /// <param name="tableIndex">表格号</param>
+        /// <param name="rowIndex">行号</param>
+        /// <param name="colIndex">列号</param>
+        /// <returns>单元格中的数据</returns>
+        public string ReadWord(Document doc, int tableIndex, int rowIndex, int colIndex)
+        {
+            //Give the value to the tow Int32 params.
+
+            try
+            {
+                var table = doc.Tables[tableIndex];
+                string text = table.Cell(rowIndex, colIndex).Range.Text.ToString();
+                text = text.Substring(0, text.Length - 2);    //去除尾部的mark 
+                return text;
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+    }
+}
